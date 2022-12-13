515b9063780ef0a5347e7019834e4e62f2e9fa64
long.phan <long.phan@netpower.no>
2022-12-13 17:05:06 +0700

    Remote fetch with mtl support

--- example/src/GestureControl.js
+++ example/src/GestureControl.js
@@ -1,5 +1,7 @@
-import React, {Component} from 'react';
-import {StyleSheet, Animated} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Animated } from 'react-native';
+import axios from 'axios';
+import * as RNFS from 'react-native-fs';
 
 import ModelView from 'react-native-gl-model-view';
 const AnimatedModelView = Animated.createAnimatedComponent(ModelView);
@@ -36,26 +38,67 @@ export default class GestureControl extends Component {
     }
   };
 
+  getContentFromUrl(url, decode = true) {
+    return axios({
+      method: 'get',
+      url,
+      responseType: 'blob',
+    }).then(
+      res => {
+        return new Promise((resolve, reject) => {
+          const fileReader = new FileReader();
+          fileReader.onloadend = () => {
+            const fileName = url.split('/').reverse()[0];
+            const filePath = `${RNFS.DocumentDirectoryPath}/${fileName}`;
+
+            if (!decode) {
+              return resolve(fileReader.result);
+            }
+            const data = fileReader.result.replace(/data:[^,]+base64,/, '')
+            RNFS.writeFile(filePath, data, 'base64');
+            resolve(`file:/${filePath}`);
+          }
+          fileReader.onerror = reject;
+          fileReader.readAsDataURL(res.data);
+        })
+      }
+    );
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData = async () => {
+    Promise.all([
+      this.getContentFromUrl('http://localhost:3001/3DModel_LowPoly.obj', false),
+      this.getContentFromUrl('http://localhost:3001/3DModel_LowPoly.mtl'),
+      this.getContentFromUrl('http://localhost:3001/3DModel_LowPoly.jpg', false)
+    ]).then(values => {
+      this.setState({ model: values[0].replace(/data:[^,]+base64,/, 'data:geometry/obj;base64,'), texture: values[1], image: values[2] })
+    }).catch(error => console.warn(error))
+  }
+
   render() {
-    let {rotateZ, rotateX, fromXY} = this.state;
+    let { rotateZ, rotateX, fromXY, model, texture, image } = this.state;
 
     return (
       <AnimatedModelView
         model={{
-          uri: 'demon.model',
+          uri: model,
         }}
         texture={{
-          uri: 'demon.png',
+          uri: image || texture,
         }}
         onStartShouldSetResponder={() => true}
         onResponderRelease={this.onMoveEnd}
         onResponderMove={this.onMove}
         animate={!!fromXY}
-        tint={{r: 1.0, g: 1.0, b: 1.0, a: 1.0}}
-        scale={0.01}
+        tint={{ r: 1.0, g: 1.0, b: 1.0, a: 1.0 }}
+        scale={1}
         rotateX={rotateX}
         rotateZ={rotateZ}
-        translateZ={-4}
+        translateZ={0}
         style={styles.container}
       />
     );
